941b2290ece37590385b17c6292893e8c7cd2795
MadMadeMadelyn <madelynching916@gmail.com>
2024-08-13 14:59:01 -0700

    Update Program3.java
    finished all length, width, area, and perimeter

--- Program3.java
+++ Program3.java
@@ -3,7 +3,26 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
+public class Program3 {
+    public static void main(String[] args){
+        int length = 143;
+        int width = 82;
+        int area = length * width;
+        int perimeter = 2 * length + 2 * width;
+        
+        Scanner numScanner = new Scanner(System.in);
+        System.out.println("Enter the length: ");
+        length = numScanner.nextInt();
+        
+        System.out.println("Enter the width: ");
+        width = numScanner.nextInt();
+        
+        System.out.println("The length is " +length);
+        System.out.println("The width is " + width);
+        System.out.println("The area is " + area);
+        System.out.println("The perimeter is " + perimeter);
+    }
+}
 
 
 
